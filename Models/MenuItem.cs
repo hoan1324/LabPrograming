40e562a37da9d975bcd68934732d7cfd048da0df
hoan1324 <hoan3397@gmail.com>
2024-08-20 20:49:03 +0700

    update

--- Models/MenuItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyWebApp.Models
-{
-    public class MenuItem
-    {
-        public int Id { get; set; }//Item id
-        public string Name { get; set; } //Item name
-        public string Link { get; set; } //Item label
-    }
-}
